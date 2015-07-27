843b6fbdadf6f3e83137c471f4afeed1d171a51e
Sean Eshbaugh <seaneshbaugh@gmail.com>
2015-07-27 14:51:16 -0500

    Initial Commit

---
+++ SystemExplorer/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  SystemExplorer
+//
+//  Created by Sean Eshbaugh on 7/27/15.
+//  Copyright (c) 2015 Sean Eshbaugh. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
